12aa55e0bd26259dbc14c536e9173a86baf4b2e0
CHERY Erwan <chery.erwan@laposte.net>
2025-03-12 13:35:20 +0100

    Implémentation de tests unitaires pour le service de remarques dans TestController
    Ajout de scénarios de test complets pour le service de remarques :
    - Synchronisation avec la base de données
    - Insertion et mise à jour de remarques avec différents motifs
    - Gestion des remarques groupées
    - Synchronisation avec le système GDS

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,13 +2,69 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Domains\Shared\Data\RemarkData;
+use App\Services\RemarkServiceInterface;
 
 class TestController extends Controller
 {
-    public function index(Request $request)
+    public function __construct(private readonly RemarkServiceInterface $remarkService) {}
+
+    public function index()
     {
-        // dd($request->all());
-        return view('test');
+        $this->remarkService->syncWithDatabase('LFPEXD', 'SABRE');
+
+        $this->remarkService->upsertRemark(
+            remark: RemarkData::from([
+                'recordLocator' => 'LFPEXD',
+                'gds' => 'SABRE',
+                'reference' => '1',
+                'text' => 'TRIPQUOTE-ID-1',
+            ]),
+            pattern: 'TRIPQUOTE-ID-*',
+        );
+
+        $this->remarkService->upsertRemark(
+            remark: RemarkData::from([
+                'recordLocator' => 'LFPEXD',
+                'gds' => 'SABRE',
+                'reference' => '1',
+                'text' => 'NO-MU-TEST',
+            ]),
+            pattern: 'NO-MU-*',
+        );
+
+        $this->remarkService->upsertRemark(
+            remark: RemarkData::from([
+                'recordLocator' => 'LFPEXD',
+                'gds' => 'SABRE',
+                'reference' => '1',
+                'text' => 'MU-TEST',
+            ]),
+            pattern: 'MU-*',
+            deletePattern: 'NO-MU-*',
+        );
+
+        $newRemarks = collect([
+            RemarkData::from([
+                'recordLocator' => 'LFPEXD',
+                'gds' => 'SABRE',
+                'reference' => '1',
+                'text' => 'PRIXA-111.1EUR/PQ1/FDOS-35.00EUR',
+            ]),
+            RemarkData::from([
+                'recordLocator' => 'LFPEXD',
+                'gds' => 'SABRE',
+                'reference' => '1',
+                'text' => 'PRIXA-111.1EUR/PQ2/FDOS-35.00EUR',
+            ]),
+        ]);
+
+        $this->remarkService->upsertRemarksWithGroupPattern(
+            newRemarks: $newRemarks,
+            groupPattern: 'PRIXA-*EUR/PQ*/FDOS-*EUR',
+            remarkPatterns: collect(['PRIXA-*EUR/PQ1/FDOS-*EUR', 'PRIXA-*EUR/PQ2/FDOS-*EUR'])
+        );
+
+        $this->remarkService->syncWithGds('LFPEXD', 'SABRE');
     }
 }
